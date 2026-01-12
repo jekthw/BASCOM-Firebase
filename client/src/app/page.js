f0eb25bc501fe7847e16bc6b675a0fd886fe324a
jekthw <mzakky.wijaya@gmail.com>
2026-01-12 10:48:16 +0700

    Pre-Migration Commit

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <div>
       <Navbar/>
-      <div className="bg-[url('/smaga.png')] h-[45rem] sm:h-[50rem] w-full bg-center bg-cover relative flex justify-center">
+      <div className="bg-[url('/smaga.png')] h-180 sm:h-200 w-full bg-center bg-cover relative flex justify-center">
         <div className="w-full px-6 md:px-0 md:w-[80vw] h-full flex flex-col gap-6 pb-20 font-poppins justify-center absolute  translate-y-18">
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl sm:text-5xl ">
@@ -37,7 +37,7 @@ export default function Home() {
                 <Article 
                 date={"10 Oktober 2025"} 
                 text={"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet"}
-                img={`/smaga.jpg`}/>
+                img={`/smaga.png`}/>
               </div>
 
               <div>
@@ -51,7 +51,7 @@ export default function Home() {
                 <Article 
                 date={"10 Oktober 2025"} 
                 text={"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet"}
-                img={`/banger.png`}/>
+                img={`/smaga.png`}/>
               
               </div>
 
@@ -59,7 +59,7 @@ export default function Home() {
                 <Article 
                 date={"10 Oktober 2025"} 
                 text={"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet"}
-                img={`/smaga.jpg`}/>
+                img={`/smaga.png`}/>
               </div>
 
               <div>
@@ -73,7 +73,7 @@ export default function Home() {
                 <Article 
                 date={"10 Oktober 2025"} 
                 text={"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet"}
-                img={`/banger.png`}/>
+                img={`/smaga.png`}/>
               
               </div>
 
